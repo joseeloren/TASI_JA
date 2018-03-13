a7f5e01daa34d1232c57b0d23d8d9a50819320ce
Jose Lorenzo <isleo11@gmail.com>
2018-03-13 10:26:37 +0000

    Cambios

--- instalacion.sh
+++ instalacion.sh
@@ -4,22 +4,20 @@ rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
 rpm -Uvh https://mirror.webtatic.com/yum/el6/latest.rpm
 
 # Instalamos los paquetes del sistema
-yum update
-yum install httpd mysql-server mod_php71w php71w-opcache -y
+yum update -y
+yum install -y httpd mysql-server mod_php71w php71w-opcache php71w-mysql
 
 # Hacemos que los servicios arranquen con el sistema
 chkconfig httpd on
 chkconfig mysqld on
 
-# Arrancamos los servicios
-service httpd start
-service mysqld start
-
 # Creamos la base de datos
+service mysqld start
 cat crear_bd.sql | mysql
 
 # Instalamos Joomla
 cd /var/www/html
 wget http://dev.virtuemart.net/attachments/download/1112/VirtueMart3.2.12_Joomla_3.8.3-Stable-Full_Package.zip 
 unzip VirtueMart3.2.12_Joomla_3.8.3-Stable-Full_Package.zip
-
+chown -R apache:apache *
+service httpd start
