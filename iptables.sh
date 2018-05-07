d79820915d4ce60d0d896bee7527442af308d470
Jose Lorenzo <isleo11@gmail.com>
2018-05-07 16:17:08 +0100

    Cambios

--- iptables.sh
+++ iptables.sh
@@ -1,6 +1,7 @@
 service iptables restart
 iptables -F
 iptables -A INPUT -p tcp -m multiport --dports 22,80,443,5901,9418 -j ACCEPT
+iptables -A INPUT -p tcp -m multiport --sports 9418 -j ACCEPT
 iptables -A INPUT -j DROP
-iptables -A FORWARD -j DROP
+iptables -A FORWARD -j ACCEPT
 iptables -A OUTPUT -j ACCEPT
